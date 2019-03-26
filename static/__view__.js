f4110e548696a34bf4a3520627f57eac69ae1d67
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-03-26 11:32:35 -0700

    Register config/theme.

--- static/__view__.js
+++ static/__view__.js
@@ -1,6 +1,7 @@
 var _vegalite,
     _vega,
-    _opt;
+    _opt,
+    _config;
 
 // TODO: validation
 
@@ -8,31 +9,34 @@ function params(options) {
   return Object.assign({}, _opt, options);
 }
 
-export async function render(options) {
+export async function render(options, config) {
   const opt = params(options);
 
   if (!opt.container) {
     opt.container = document.createElement('div');
   }
 
-  const view = await toView.call(this, opt).runAsync(),
+  const view = await toView.call(this, opt, config).runAsync(),
         div = view.container() || {};
 
   div.value = view;
   return div;
 }
 
-export function toView(options) {
+export function toView(options, config) {
   if (!_vegalite || !_vega) {
     throw Error('Vega / Vega-Lite not registered. Use the "register" method.');
   }
-  const spec = _vegalite.compile(this.toJSON());
-  return new _vega.View(_vega.parse(spec.spec), params(options));
+  const spec = _vegalite.compile(this.toJSON()),
+        runtime = _vega.parse(spec.spec, config || _config);
+
+  return new _vega.View(runtime, params(options));
 }
 
-export function register(vega, vegalite, options) {
+export function register(vega, vegalite, options, config) {
   _vega = vega;
   _vegalite = vegalite;
   _opt = options;
+  _config = config;
   return this;
 }
