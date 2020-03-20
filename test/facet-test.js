3e055dc355d5b2fcf127f94dca1bd105b47c7a23
Jeffrey Heer <jheer@trifacta.com>
2020-03-20 11:01:27 +0100

    Add test cases for facet operator.

---
+++ test/facet-test.js
@@ -0,0 +1,67 @@
+const tape = require('tape'),
+      vl = require('../');
+
+function equalSpec(t, api, spec) {
+  t.equal(JSON.stringify(api.toJSON()), JSON.stringify(spec));
+}
+
+tape('Faceting unit spec is supported', function(t) {
+  const values = [{key: 'a'}, {key: 'b'}];
+
+  // data provided within unit spec
+  const facet1 = vl.markLine().data(values).facet(vl.fieldN('key'));
+  const spec1 = {
+    facet: {field: 'key', type: 'nominal'},
+    spec: { mark: {type: 'line'}, data: {values} }
+  };
+  equalSpec(t, facet1, spec1);
+
+  // data provided outside of unit spec
+  const facet2 = vl.markLine().facet(vl.fieldN('key')).data(values);
+  const spec2 = {
+    facet: {field: 'key', type: 'nominal'},
+    spec: { mark: {type: 'line'} },
+    data: {values}
+  };
+  equalSpec(t, facet2, spec2);
+
+  t.end();
+});
+
+tape('Faceting composite spec is supported', function(t) {
+  const values = [{key: 'a'}, {key: 'b'}];
+
+  // data provided within composite spec
+  const facet1 = vl.data(values)
+    .layer(vl.markLine(), vl.markBar())
+    .facet(vl.fieldN('key'));
+  const spec1 = {
+    facet: {field: 'key', type: 'nominal'},
+    spec: {
+      layer: [
+        {mark: {type: 'line'}},
+        {mark: {type: 'bar'}}
+      ],
+      data: {values},
+    }
+  };
+  equalSpec(t, facet1, spec1);
+
+  // data provided outside of composite spec
+  const facet2 = vl.layer(vl.markLine(), vl.markBar())
+    .facet(vl.fieldN('key'))
+    .data(values);
+  const spec2 = {
+    facet: {field: 'key', type: 'nominal'},
+    spec: {
+      layer: [
+        {mark: {type: 'line'}},
+        {mark: {type: 'bar'}}
+      ]
+    },
+    data: {values}
+  };
+  equalSpec(t, facet2, spec2);
+
+  t.end();
+});
